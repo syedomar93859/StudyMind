e9bce64172425fe2f94f8af6f593c6cc986a5be2
Syed Omar <syed.omar@ucalgary.ca>
2026-05-12 23:41:07 -0600

    use API key to connect with a gemini models and get a response

---
+++ backend/AI-setup.js
@@ -0,0 +1,35 @@
+import { GoogleGenAI } from '@google/genai';
+import 'dotenv/config';
+
+// Create an AI client object
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
+// Configuration for the model
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+};
+
+async function run() {
+    try {
+        //gemini gives a response
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: "Explain what Node.js is in simple words.",
+            generationConfig,
+        });
+
+        // print AI response
+        console.log(response.text);
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+run();
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import http from 'http';
+// this imports Node.js’s built-in HTTP module
+// this module allows Node.js to: create web servers, receive browser requests and send responses back
+
 import fs from 'fs';
+// imports Node.js’s File System module
+// The fs module allows Node to: read files, write files, delete files and modify files
 
 const port = 3000;
+// stores the port number
+// localhost:3000 means to "Connect to my own computer on port 3000"
+
 const server = http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type' : 'text/html'});
     fs.readFile('../frontend/index.html',function(error, data){
@@ -13,9 +21,37 @@ const server = http.createServer(function(req,res){
         }
         res.end();
     })
-    // res.end("Hello! This is a simple Node.js server.");
-    // read.write("Hello Node");
 })
+// line 13 creates the web server
+// function(req,res){ runs every time someone visits your server
+
+// req means request object
+// contains information about the incoming browser request
+// Examples: URL visited, request method (GET/POST), headers
+
+// res means response object
+// Used to send data back to the browser
+
+// line 14 sends HTTP response headers
+// 200 is the HTTP status code which means success
+
+// 'Content-Type' tells the browser "What kind of data am I sending?"
+
+// 'text/html' means: "This response contains HTML"
+// so the browser renders it as a webpage.
+
+// line 15 reads the HTML file asynchronously
+// "Start reading the file, tell me when finished"
+// ../frontend/index.html' means:
+// go up one folder
+// then enter frontend
+// then open index.html
+
+// data contains the contents of index.html
+
+// res.write(data) sends the contents of index.html to the browser.
+
+// res.end() finishes the response
 
 server.listen(port, function(error){
     if (error){
