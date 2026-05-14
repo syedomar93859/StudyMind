e913f1b55b16ce630e8f5120cff6045dc4508726
Syed Omar <syed.omar@ucalgary.ca>
2026-05-13 23:26:49 -0600

    changing elements and making the website more interactive

--- frontend/main.js
+++ frontend/main.js
@@ -1,30 +1,13 @@
+const button = document.getElementById('submit')
 
-// Stores if submit button has been selected
-// A Set is used so the same language cannot be added twice
-const submitStatus = new Set();
+button.addEventListener("click", doSomething);
 
-// const submitStatus = true
+function doSomething(){
+    const input = document.getElementById('submission').value;
 
-function toggleSubmit(button) {
-
-    // Retrieve the language stored in the button's data-language attribute
-    const status = button.dataset.language;
-
-    if (submitStatus.has(status)) {
-
-        // Remove language if already selected
-        submitStatus.delete(status);
-
-        // Remove visual highlight from button
-        button.classList.remove("active");
-
-    } else {
-
-        // Add language to the selection
-        submitStatus.add(status);
-
-        // Visually mark button as active
-        button.classList.add("active");
+    if (input.length == 0){
+        alert("Input box is empty!");
+    } else{
+        alert("Input box is not empty!");
     }
-    
 }
