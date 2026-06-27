f5344b58a2f4d6954cfa84afd9f691ce3618958a
Syed Omar <syed.omar@ucalgary.ca>
2026-06-26 22:30:08 -0600

    fixing a few design issues with the login page

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -7,25 +7,64 @@ const signUpBtn = document.querySelector("#signUpBtn");
 const signInBtn = document.querySelector("#signInBtn");
 
 function loginFunction(){
+
+    // move the login form into the center
     loginForm.style.left = "50%";
     loginForm.style.opacity = 1;
+
+    // slide the register form off-screen to the right
     registerForm.style.left = "150%";
     registerForm.style.opacity = 0;
+
+    // resize the wrapper to fit the login form
     wrapper.style.height = "500px";
+
+    // show the Login title
     loginTitle.style.top = "50%";
     loginTitle.style.opacity = 1;
+
+    // hide the Register title
     registerTitle.style.top = "50px";
     registerTitle.style.opacity = 0;
 }
 
 function registerFunction(){
+
+    // slide the login form off-screen to the left
     loginForm.style.left = "-50%";
     loginForm.style.opacity = 0;
+
+    // move the register form into the center
     registerForm.style.left = "50%";
     registerForm.style.opacity = 1;
+
+    // increase the wrapper height for the larger register form
     wrapper.style.height = "580px";
+
+    // hide the Login title
     loginTitle.style.top = "-60px";
     loginTitle.style.opacity = 0;
+
+    // show the Register title
     registerTitle.style.top = "50%";
     registerTitle.style.opacity = 1;
 }
+
+document.getElementById("signUpBtn").addEventListener("click", function() {
+  const regName = document.getElementById("reg-name").value;
+  const regEmail = document.getElementById("reg-email").value;
+  const regPass = document.getElementById("reg-pass").value;
+  const agreed = document.getElementById("agree").checked;
+
+  if (regName != "" && regEmail != "" && regPass != "" && agreed) {
+    alert("Your username is " + regName + " and your email is " + regEmail + " and your password is " + regPass + " and you have checked the box");
+  }
+});
+
+document.getElementById("signInBtn").addEventListener("click", function() {
+    const logEmail = document.getElementById("log-email").value;
+    const logPass = document.getElementById("log-pass").value;
+    if (logEmail!= "" && logPass != "") {
+        alert("Your email is " + logEmail + " and your password is " + logPass);
+}
+});
